a2334edec75530319139e63293658aadb5cc16db
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-23 14:30:14 +0530

    chore:save current work before branching

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Button } from "../ui/button"
 import Link from "next/link"
-export default function Navbar(){
+export default function Navbar() {
     return <nav className=" border-b border-purple-800 flex items-center justify-between px-4 py-3 w-full">
-        <h1 className="font-inter text-3xl text-purple-700">Cornverge</h1>
+        <div className="flex items-center text-3xl text-purple-600 font-semibold">    
+           <h1>Cornverge</h1>
+        </div>
+        
         <Button variant={"ghost"} className="bg-purple-700 hover:bg-purple-800 text-white text-lg hover:text-white cursor-pointer">
             <Link href={"/auth/login"}>Start</Link>
         </Button>
