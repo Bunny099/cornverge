a180a963919ab5763dc5fb95d22d4500d221e158
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-28 19:04:01 +0530

    UI:updated nav and footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function Footer(){
+export default function Footer() {
   return (
-    <footer className="flex flex-col py-6 border-t border-gray-300">
-      <div className="flex justify-center gap-6 text-sm text-gray-500 mb-2">
-        <Link href="/privacy" className="hover:text-purple-700 transition-colors">
+    <footer className="flex flex-col md:flex-row justify-between px-4 py-4 border-t border-border">
+      <div className="flex justify-center gap-5 text-sm text-foreground mb-2">
+        <Link href="/privacy" className="hover:text-primary transition-colors">
           Privacy Policy
         </Link>
-        <Link href="/terms" className="hover:text-purple-700 transition-colors">
+        <Link href="/terms" className="hover:text-primary transition-colors">
           Terms of Service
         </Link>
       </div>
-      <p className="text-center text-gray-400 text-xs">
+      <p className="text-center text-muted-foreground text-xs">
         © {new Date().getFullYear()} Cornverge. All rights reserved.
       </p>
     </footer>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,18 +1,22 @@
-import { Button } from "../ui/button"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ModeToggle } from "./toggle";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-30 flex items-center justify-between px-8 md:px-12 py-4 backdrop-blur-sm bg-white/70 shadow-sm">
-      <div className="flex items-center text-lg md:text-xl font-extrabold text-black tracking-wider">
-        <span className="text-purple-700">CORNVERGE</span>
+    <nav className="sticky top-0 z-50 flex items-center justify-between px-3 md:px-12 py-4 backdrop-blur-sm bg-card/70 shadow-md transition-colors duration-300">
+      <div className="text-2xl font-extrabold tracking-wide text-foreground">
+        <span className="text-primary">CORNVERGE</span>
       </div>
-
+      <div className="flex items-center gap-2">
+        <ModeToggle />
       <Link href="/auth/login">
-        <Button className="bg-black text-white px-4 py-2 rounded-full text-sm hover:bg-purple-700 cursor-pointer transition-all">
+        <Button className=" px-5 py-2 rounded-full hidden md:block transition-all duration-300 shadow-md cursor-pointer">
           START
         </Button>
       </Link>
+      </div>
+      
     </nav>
-  )
+  );
 }
